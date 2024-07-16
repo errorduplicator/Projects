c973d2ee301b6fec67e5dabce3bbb1194373dd8f
errorduplicator <135431383+errorduplicator@users.noreply.github.com>
2024-07-16 02:21:54 +0000

    update get_f10.py add save.txt

--- news_acqusition_project/get_f10.py
+++ news_acqusition_project/get_f10.py
@@ -9,6 +9,9 @@ import re
 import pandas as pd
 
 list_ = open('list.txt','r')
+save = open('save.txt', 'r+')
+start = time.time()
+end = 0.0
 events = []
 links = []
 data = {'event' : events,
@@ -16,35 +19,41 @@ data = {'event' : events,
 driver = webdriver.Chrome()
 lines = list_.readlines()
 for line in range(5362):
-    code = lines[line]
-    driver.get(''.join(['https://basic.10jqka.com.cn/', code, '/news.html']))
+    if end < 1800:
+        code = lines[line]
+        driver.get(''.join(['https://basic.10jqka.com.cn/', code, '/news.html']))
 
-    driver.implicitly_wait(20)
-    m_box_subnews = driver.find_element(By.ID, "pub")
-    page_link_box = m_box_subnews.find_element(By.ID, "splpager_page")
-    page_links = page_link_box.find_elements(By.CLASS_NAME, "page-link")
-    next_page = page_links[-1]
-    for page in range(1, 20):
-        driver.implicitly_wait(3)
+        driver.implicitly_wait(20)
         m_box_subnews = driver.find_element(By.ID, "pub")
         page_link_box = m_box_subnews.find_element(By.ID, "splpager_page")
         page_links = page_link_box.find_elements(By.CLASS_NAME, "page-link")
         next_page = page_links[-1]
-        elements = m_box_subnews.find_elements(By.CLASS_NAME, "client")
-        for element in elements:
-            WebDriverWait(driver, 40, ignored_exceptions=(NoSuchElementException,StaleElementReferenceException)).until(EC.element_to_be_clickable(element))
-            event_name = element.text
-            keywords = ['证监会', '中国证券监督管理委员会', '罚', '退市']
-            combined_pattern = '|'.join(keywords)
-            matches = re.findall(combined_pattern, event_name)
-            if matches:
-                link = element.get_attribute('href')
-                events.append(event_name + '\n')
-                links.append(link + '\n')
-        WebDriverWait(driver, 40, ignored_exceptions=(NoSuchElementException,StaleElementReferenceException)).until(EC.element_to_be_clickable(next_page))
-        next_page.click()
-        time.sleep(3)
-        print(page)
+        for page in range(1, 20):
+            
+            driver.implicitly_wait(3)
+            m_box_subnews = driver.find_element(By.ID, "pub")
+            page_link_box = m_box_subnews.find_element(By.ID, "splpager_page")
+            page_links = page_link_box.find_elements(By.CLASS_NAME, "page-link")
+            next_page = page_links[-1]
+            elements = m_box_subnews.find_elements(By.CLASS_NAME, "client")
+            for element in elements:
+                WebDriverWait(driver, 40, ignored_exceptions=(NoSuchElementException,StaleElementReferenceException)).until(EC.element_to_be_clickable(element))
+                event_name = element.text
+                keywords = ['证监会', '中国证券监督管理委员会', '罚', '退市']
+                combined_pattern = '|'.join(keywords)
+                matches = re.findall(combined_pattern, event_name)
+                if matches:
+                    link = element.get_attribute('href')
+                    save.write(event_name + '\n')
+                    save.write(link + '\n')
+            current_time = time.time()
+            WebDriverWait(driver, 40, ignored_exceptions=(NoSuchElementException,StaleElementReferenceException)).until(EC.element_to_be_clickable(next_page))
+            next_page.click()
+            time.sleep(3)
+    else:
+        save.write(line, '\n', page, '\n')
+list_.close()
+save.close()
 df = pd.DataFrame(data)
 df.to_excel('a.xlsx', index=False, header=False)
 driver.quit()
