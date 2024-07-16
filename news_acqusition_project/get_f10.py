5325ca99264e5d4e62d9294fe007ee655bb9ef09
errorduplicator <135431383+errorduplicator@users.noreply.github.com>
2024-07-16 06:07:02 +0000

    update

--- news_acqusition_project/get_f10.py
+++ news_acqusition_project/get_f10.py
@@ -10,27 +10,50 @@ import pandas as pd
 
 list_ = open('list.txt','r') # open files
 save = open('save.txt', 'r+') # open files
-start = time.time()
+driver = webdriver.Edge() # start browser
+start = time.time() # start timing
 end = 0.0
-events = []
-links = []
-data = {'event' : events,
-        'link' : links}
-driver = webdriver.Chrome()
+page = 0
 lines = list_.readlines()
+cached = save.readlines()
+if cached:
+    if cached[-2]==5362 and cached[-1]==20:
+        events = []
+        links = []
+        data = {'event' : events,
+        'link' : links}
+        with open('save.txt', 'r') as file:
+            ls = file.readlines() # Remove the last two lines
+            ls = ls[:-2] # Write the remaining lines back to the file
+        with open('save.txt', 'w') as file:
+            file.writelines(ls)
+        save = open('save.txt','r')
+        cached = save.readlines()
+        for l in range(len(cached)):
+            if l % 2 == 0:
+                events.append(cached[l])
+            else:
+                links.append(cached[l])
+        exit()
 for line in range(5362):
-    if end < 1800:
-        save.readlines()
+    if end-start < 1800:
+        if cached:
+            code  = cached[-1]
+        else:
+            code = lines[line]
         code = lines[line]
         driver.get(''.join(['https://basic.10jqka.com.cn/', code, '/news.html']))
-
         driver.implicitly_wait(20)
         m_box_subnews = driver.find_element(By.ID, "pub")
         page_link_box = m_box_subnews.find_element(By.ID, "splpager_page")
         page_links = page_link_box.find_elements(By.CLASS_NAME, "page-link")
         next_page = page_links[-1]
-        for page in range(1, 20):
-            
+        t = 0
+        if t < 1 and cached:
+            page = cached[-2]
+        else:
+            page = 1
+        while page <= 20:
             driver.implicitly_wait(3)
             m_box_subnews = driver.find_element(By.ID, "pub")
             page_link_box = m_box_subnews.find_element(By.ID, "splpager_page")
@@ -50,11 +73,17 @@ for line in range(5362):
             current_time = time.time()
             WebDriverWait(driver, 40, ignored_exceptions=(NoSuchElementException,StaleElementReferenceException)).until(EC.element_to_be_clickable(next_page))
             next_page.click()
-            time.sleep(3)
+            page += 1
+        time.sleep(3)
     else:
+        with open('save.txt', 'r') as file:
+            ls = file.readlines() # Remove the last two lines
+            ls = ls[:-2] # Write the remaining lines back to the file
+        with open('save.txt', 'w') as file:
+            file.writelines(ls)
         save.write(line, '\n', page, '\n')
+driver.quit()
+    
+    
 list_.close()
 save.close()
-df = pd.DataFrame(data)
-df.to_excel('a.xlsx', index=False, header=False)
-driver.quit()
--- news_acqusition_project/test.py
+++
@@ -1,4 +0,0 @@
-with open('temp.txt', 'r+') as f:
-    print(f.read())
-    print(f.write('32262\n'))
-    print(f.write('af'))
