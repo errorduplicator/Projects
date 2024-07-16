27d957403dc3d1c9524ae80a71f73da7643a8a76
errorduplicator <135431383+errorduplicator@users.noreply.github.com>
2024-07-16 08:37:59 +0000

    test

--- news_acqusition_project/test.py
+++ news_acqusition_project/test.py
@@ -1,9 +1,10 @@
 save = open('save.txt', 'r+')
 cached = save.readlines()
 
-while len(cached)<=1548415364:
-    if cached[1548415364]==2:
-        print('boo ya')
+while True:
+    if len(cached)>1548415364:
+        if cached[1548415364]==2:
+            print('boo ya')
     else:
         cached.append(2)
     print(cached)
