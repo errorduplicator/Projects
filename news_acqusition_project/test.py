a9d34baa4d990c7364cfcfa65639e43a576f49fb
errorduplicator <135431383+errorduplicator@users.noreply.github.com>
2024-07-16 08:21:19 +0000

    testing

---
+++ news_acqusition_project/test.py
@@ -0,0 +1,9 @@
+save = open('save.txt', 'r+')
+cached = save.readlines()
+
+while cached:
+    if cached[1548415364]==2:
+        print('boo ya')
+    else:
+        cached.append(2)
+    print(cached)
